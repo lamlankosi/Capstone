2dab7620bff4d11af4d18571f7852e52d3d78b52
lamlankosi <lamlanomnganga9@gmail.com>
2024-08-29 14:37:10 +0200

    Middleware

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ import path from 'path'
 import cors from 'cors'
 import 'dotenv/config'
 import { express, userRouter } from './controller/userController.js'
+import {productRouter} from './controller/prodController.js'
 
 const app = express()
 const port = +process.env.PORT || 4000
 
-app.use('/user', userRouter)
+//middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+
+    next()
+})
+
+app.use('/users', userRouter)
+app.use('/products', productRouter )
 app.use(
     express.static('./static'),
     express.json(),
