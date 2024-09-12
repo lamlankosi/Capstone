d2c799fb45954ab71f0fda3d7a304e6f494278a1
lamlankosi <lamlanomnganga9@gmail.com>
2024-09-12 16:22:57 +0200

    updated edit order

--- model/Orders.js
+++ model/Orders.js
@@ -1,117 +1,123 @@
-import {connection as db } from '../config/index.js'
+import { connection as db } from "../config/index.js";
 
-class Orders{
-    fetchOrders(req,res){
-       try{
-        const strQry =`
-        SELECT o.orderID, o.orderDate, u.firstName, u.emailAdd, p.prodName,o.totalAmount,u.Location, o.status
-        FROM Orders o
-        INNER JOIN Users u ON o.userID = u.userID
-        INNER JOIN Products p ON o.prodID = p.prodID;
-        `
-        db.query(strQry, (err, results)=>{
-            if(err) throw new Error(err)
-                res.json({
-            status: res.statusCode,
-            results
-        })
-        })
-       }catch(e){
+class Orders {
+  fetchOrders(req, res) {
+    try {
+      const strQry = `
+        SELECT o.orderID, CONCAT(SUBSTRING(o.orderDate, 1, 10), SUBSTRING(orderDate, 11, 12)) 'orderDate',
+    u.firstName,
+    u.emailAdd,
+	p.prodName,
+    o.totalAmount,
+    u.Location,
+    o.status
+FROM 
+    Orders o
+INNER JOIN 
+    Users u ON o.userID = u.userID
+INNER JOIN 
+    Products p ON o.prodID = p.prodID;
+        `;
+      db.query(strQry, (err, results) => {
+        if (err) throw new Error(err);
         res.json({
-            status:404,
-            msg:e.message
-        })
-       } 
+          status: res.statusCode,
+          results,
+        });
+      });
+    } catch (e) {
+      res.json({
+        status: 404,
+        msg: e.message,
+      });
     }
-    fetchOrder(req,res){
-    try{
-        const strQry=`
-        SELECT o.orderID, o.orderDate, u.firstName, u.emailAdd, p.prodName,o.totalAmount,u.Location, o.status
+  }
+  fetchOrder(req, res) {
+    try {
+      const strQry = `
+        SELECT o.orderID, CONCAT(SUBSTRING(o.orderDate, 1, 10), SUBSTRING(orderDate, 11, 12)) 'orderDate', u.firstName, u.emailAdd, p.prodName,o.totalAmount,u.Location, o.status
         FROM Orders o
         INNER JOIN Users u ON o.userID = u.userID
         INNER JOIN Products p ON o.prodID = p.prodID
         WHERE orderID=${req.params.id}
-        `
-        db.query(strQry, (err, result) =>{
-            if(err) throw new Error (err)
-                res.json({
-                    status:res.statusCode,
-                    result
-                })
-        })
-    } catch(e){
+        `;
+      db.query(strQry, (err, result) => {
+        if (err) throw new Error(err);
         res.json({
-            status:404,
-            msg: e.message
-        })
+          status: res.statusCode,
+          result,
+        });
+      });
+    } catch (e) {
+      res.json({
+        status: 404,
+        msg: e.message,
+      });
     }
-    }
-    addOrder(req,res){
-        try{
-            let data = req.body
-            const strQry=`
+  }
+  addOrder(req, res) {
+    try {
+      let data = req.body;
+      const strQry = `
             INSERT INTO Orders
             SET ?
-            `
-            db.query(strQry, [data],(err) => {
-                if (err) throw new Error(err)
-                    res.json({
-                        status: res.statusCode,
-                        msg: 'You have placed an order'
-                    })
-            })
-        } catch(e){
-            res.json({
-                status: 404,
-                msg: e.message
-            })
-        }
+            `;
+      db.query(strQry, [data], (err) => {
+        if (err) throw new Error(err);
+        res.json({
+          status: res.statusCode,
+          msg: "You have placed an order",
+        });
+      });
+    } catch (e) {
+      res.json({
+        status: 404,
+        msg: e.message,
+      });
     }
-    updateOrder(req,res){
-        try{
-            let data = req.body
-            const strQry = `
+  }
+  updateOrder(req, res) {
+    try {
+      let data = req.body;
+      const strQry = `
             UPDATE Orders
             SET ?
             WHERE orderID = ${req.params.id}
-            `
-            db.query(strQry, [data], (err) =>{
-                if(err) throw new Error(err)
-                    res.json({
-                        status:res.statusCode,
-                        msg:'You have update the order'
-                    })
-            })
-        }catch(e){
-            res.json({
-                status: 404,
-                msg: e.message
-            })
-        }
+            `;
+      db.query(strQry, [data], (err) => {
+        if (err) throw new Error(err);
+        res.json({
+          status: res.statusCode,
+          msg: "You have update the order",
+        });
+      });
+    } catch (e) {
+      res.json({
+        status: 404,
+        msg: e.message,
+      });
     }
-    deleteOrder(req,res) {
-        try{
-            const strQry=`
+  }
+  deleteOrder(req, res) {
+    try {
+      const strQry = `
             DELETE
             FROM Orders
-            WHERE orderID = ${req.params.id};`
-            db.query(strQry, (err)=>{
-                if(err) throw new Error (err)
-                    res.json({
-                        status: res.statusCode,
-                        msg: 'Order has been deleted'
-                    })
-            })
-        } catch (e) {
-            res.json({
-                status:404,
-                msg: e.message
-            })
-        }
+            WHERE orderID = '${req.params.id}';`;
+      db.query(strQry, (err) => {
+        if (err) throw new Error(err);
+        res.json({
+          status: res.statusCode,
+          msg: "Order has been deleted",
+        });
+      });
+    } catch (e) {
+      res.json({
+        status: 404,
+        msg: e.message,
+      });
     }
-
+  }
 }
 
-export {
-    Orders
-}
+export { Orders };
